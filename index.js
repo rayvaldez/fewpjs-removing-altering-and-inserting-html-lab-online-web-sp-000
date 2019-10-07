60ef889068dfbbeaf441a9c67440c663c5f3a06d
Ray Valdez <ray_valdez@hotmail.co.uk>
2019-10-07 16:50:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+document.querySelector('main#main').remove()
+
+let header = document.createElement("h1");
+
+header.id = 'victory';
+
+header.innerHTML = "RAY is the champion"
+
+document.body.appendChild(header);
+
+let newHeader = document.querySelector("h1#victory")
