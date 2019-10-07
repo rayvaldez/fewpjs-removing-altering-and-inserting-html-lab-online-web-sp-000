ef7f36823b18222ee63fc1a9c276cc750e9a4880
Ray Valdez <ray_valdez@hotmail.co.uk>
2019-10-07 16:32:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ main.remove()
 
 let newHeader = document.createElement('h1')
 
-let newHeader = document.querySelector("h1#victory")
+let newHeader = document.querySelector(h1#victory)
 
 newHeader.innerHTML = "RAY is the champion"
